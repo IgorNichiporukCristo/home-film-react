0f8e6d9a7265535355b61ed2cef9a52399d07470
IgorNichiporukCristo <inichiporuk1992@gmail.com>
2019-02-17 23:37:09 +0300

    +edit config

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
 
-class App extends Component {
-    render(){
-        return(
+export default class App extends Component {
+    render () {
+        return (
             <div>
                 <h1>Здмывм</h1>
             </div>
         )
     }
 }
-
-export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,47 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const webpack = require('webpack')
-const Fiber = require('fibers')
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
     mode: 'development',
-    entry: 
-    [   'webpack-hot-middleware/client',
-        './src/index.js'],
+    entry: [
+        'webpack-hot-middleware/client',
+        './src/index.js'
+    ],
     output: {
         path: path.join(__dirname, '/dist'),
         filename: 'index_bundle.js'
     },
-       devServer: {
-        overlay: true
+    devServer: {
+        overlay: true,
+        contentBase: './dist',
+        hot: true
     },   
     module: {
         rules: [
             {
                 test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
-                use :{
-                    loader: 'babel-loader'
-                }
+                use: ['babel-loader']
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    "style-loader", 
+                    "css-loader", 
+                    "sass-loader" 
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
             }
         ]
     },
     plugins: [
         new HtmlWebpackPlugin({
             template: './src/index.html'
-        })
-    ],
-    module: {
-        rules: [{
-            test: /\.scss$/,
-            use: [
-                "style-loader", 
-                "css-loader", 
-                "sass-loader" 
-            ]
-        }]
-    }
+        }),
+        new webpack.HotModuleReplacementPlugin(),
+    ] 
 }
