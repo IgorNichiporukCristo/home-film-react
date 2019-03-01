ef54488d030110d5fefa2cc774152b3aff02607e
IgorNichiporukCristo <inichiporuk1992@gmail.com>
2019-03-01 04:14:39 +0300

    debug

---
+++ src/components/Header/Content/Search.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { hot } from 'react-hot-loader';
+import './HeaderContent.scss';
+
+function Search() {
+    return (
+      <div className='SEARCH'>
+        <div>
+          <h1>FILMS</h1>
+        </div>
+        <div>ih</div>
+      </div>
+    );
+  }
+
+  export default hot(module)(Search);
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { hot } from 'react-hot-loader';
+import './header.scss';
+import Search from './Content/Search';
+import DescriptionFilm from './Content/DescriptionFilm';
+
+function Header() {
+  return (
+    <div className='header'> 
+      <Search />
+      <DescriptionFilm />
+    </div>
+  );
+}
+
+export default hot(module)(Header);
