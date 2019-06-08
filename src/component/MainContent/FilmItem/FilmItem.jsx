f1e4df130b4b482657a300056f8a0d8c308048bf
IgorNichiporukCristo <inichiporuk1992@gmail.com>
2019-06-08 17:29:41 +0300

    add up window

--- src/component/MainContent/FilmItem/FilmItem.jsx
+++ src/component/MainContent/FilmItem/FilmItem.jsx
@@ -10,30 +10,65 @@ class FilmItem extends Component {
     getDescriptionFilm();
   }
 
+  ViewInfoClick(){
+
+  }
+
+  ImageItem(props){
+    return(
+      <div className='ImageItem'>
+        <img alt="" src={`https://image.tmdb.org/t/p/w500${props.image}`} />
+        <div className="children">
+          <button className='button-video' type="button">
+            <div className='triangle' />
+          </button>
+          <h4 className='text-video'>PLAY VIDEO</h4>
+          <button className='button-overview' type="button">
+            <h4>View Info</h4>
+          </button>
+        </div>
+      </div>
+    ); 
+  }
+
+  UpWindow(props){
+    return(
+      <div className='upwindow'>
+        <h4 className='titlewindow'>{props.title}</h4>
+        <h4>{props.genres}</h4>
+        <h4>{props.vote}</h4>
+      </div>
+    );
+  }
+
+  InformationItem(props){
+    return(
+      <div className='info'>
+        <div className='titlevote'>
+          <h4 className='titlestyle'>{props.title}</h4>
+          <h4 className='votestyle'>{props.vote}</h4>
+        </div>
+        <h5 className='genres'>
+          {props.genres ? props.genres.map(obj => obj.name).join(', ') : 'Loading...'}
+        </h5>
+      </div>
+    );
+  }
+
   itemConstruсtor(item) {
     return (
       <li>
-        <div className="ImageButton">
-          <img alt="" src={`https://image.tmdb.org/t/p/w500${item.poster_path}`} />
-          <div className="children">
-            <button className='button-video' type="button">
-              <div className='triangle' />
-            </button>
-            <h4 className='text-video'>PLAY VIDEO</h4>
-            <button className='button-overview' type="button">
-              <h4>View Info</h4>
-            </button>
-          </div>
-        </div>
-        <div className='info'>
-          <div className="titlevote">
-            <h4 className="titlestyle">{item.original_title}</h4>
-            <h4 className="votestyle">{item.vote_average}</h4>
-          </div>
-          <h5 className="genres">
-            {item.genres ? item.genres.map(obj => obj.name).join(', ') : 'Loading...'}
-          </h5>
-        </div>
+        <this.ImageItem image={item.poster_path} />
+        <this.InformationItem 
+          title={item.original_title} 
+          vote={item.vote_average} 
+          genres={item.genres} 
+        />
+        <this.UpWindow 
+          title={item.original_title} 
+          vote={item.vote_average} 
+          genres={item.genres}
+        />
       </li>
     );
   }
