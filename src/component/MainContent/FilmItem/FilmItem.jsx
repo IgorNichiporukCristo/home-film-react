7b68ca5bbf868c689541415d544a5c4e396e5179
IgorNichiporukCristo <inichiporuk1992@gmail.com>
2019-06-11 12:39:09 +0300

    added component

--- src/component/MainContent/FilmItem/FilmItem.jsx
+++ src/component/MainContent/FilmItem/FilmItem.jsx
@@ -4,78 +4,83 @@ import { connect } from 'react-redux';
 import { getDescriptionFilm } from '../../action/fetchFilms';
 import './listStyle.scss';
 
+const ImageItem = ({ image }) => (
+  <div className='image-container'>
+    <img className='image' alt='' src={`https://image.tmdb.org/t/p/w500${image}`} />
+    <div className='image-button-container'>
+      <button className='image-button-video' type='button'>
+        <div className='image-button-triangle' />
+      </button>
+      <h4 className='image-text'>play video</h4>
+      <button className='image-overview' type="button">
+        <h4>View Info</h4>
+      </button>
+    </div>
+  </div>
+);
+
+ImageItem.propTypes = {
+  image: PropTypes.string.isRequired,
+};
+
 class FilmItem extends Component {
   componentDidMount() {
     const { getDescriptionFilm } = this.props;
     getDescriptionFilm();
   }
+  
 
-  ViewInfoClick(){
+  // UpWindow(props){
+  //   return(
+  //     <div className='upwindow'>
+  //       <h4 className='titlewindow'>{props.title}</h4>
+  //       <h4>{props.genres}</h4>
+  //       <h4>{props.vote}</h4>
+  //     </div>
+  //   );
+  // }
 
-  }
+  // InformationItem(props){
+  //   return(
+  //     <div className='info'>
+  //       <div className='titlevote'>
+  //         <h4 className='titlestyle'>{props.title}</h4>
+  //         <h4 className='votestyle'>{props.vote}</h4>
+  //       </div>
+  //       <h5 className='genres'>
+  //         {props.genres ? props.genres.map(obj => obj.name).join(', ') : 'Loading...'}
+  //       </h5>
+  //     </div>
+  //   );
+  // }
 
-  ImageItem(props){
-    return(
-      <div className='ImageItem'>
-        <img alt="" src={`https://image.tmdb.org/t/p/w500${props.image}`} />
-        <div className="children">
-          <button className='button-video' type="button">
-            <div className='triangle' />
-          </button>
-          <h4 className='text-video'>PLAY VIDEO</h4>
-          <button className='button-overview' type="button">
-            <h4>View Info</h4>
-          </button>
-        </div>
-      </div>
-    ); 
-  }
+  // itemConstruсtor(item) {
+  //   return (
+  //     <li>
+        
+  //       {/* <this.InformationItem
+  //         key={item.id}
+  //         title={item.original_title} 
+  //         vote={item.vote_average} 
+  //         genres={item.genres} 
+  //       />
+  //       <this.UpWindow 
+  //         key={item.id}
+  //         title={item.original_title} 
+  //         vote={item.vote_average} 
+  //         genres={item.genres}
+  //       /> */}
+  //     </li>
+  //   );
+  // }
 
-  UpWindow(props){
-    return(
-      <div className='upwindow'>
-        <h4 className='titlewindow'>{props.title}</h4>
-        <h4>{props.genres}</h4>
-        <h4>{props.vote}</h4>
-      </div>
-    );
-  }
-
-  InformationItem(props){
-    return(
-      <div className='info'>
-        <div className='titlevote'>
-          <h4 className='titlestyle'>{props.title}</h4>
-          <h4 className='votestyle'>{props.vote}</h4>
-        </div>
-        <h5 className='genres'>
-          {props.genres ? props.genres.map(obj => obj.name).join(', ') : 'Loading...'}
-        </h5>
-      </div>
-    );
-  }
-
-  itemConstruсtor(item) {
+  render () {
+    const { item } = this.props;
     return (
       <li>
-        <this.ImageItem image={item.poster_path} />
-        <this.InformationItem 
-          title={item.original_title} 
-          vote={item.vote_average} 
-          genres={item.genres} 
-        />
-        <this.UpWindow 
-          title={item.original_title} 
-          vote={item.vote_average} 
-          genres={item.genres}
-        />
+        <ImageItem image={item.poster_path} />
       </li>
     );
-  }
-
-  render() {
-    const { item } = this.props;
-    return this.itemConstruсtor(item);
   }
 }
 
