07a9a33a0047981da9aa75217ee81d8878319aaf
IgorNichiporukCristo <inichiporuk1992@gmail.com>
2019-06-26 23:03:38 +0300

    stylization video countainer

--- src/component/MainContent/FilmItem/Video.jsx
+++ src/component/MainContent/FilmItem/Video.jsx
@@ -6,20 +6,23 @@ import './video.scss';
 
 class Video extends Component {
 
+  componentDidMount() {
+    this.root = document.createElement('div');
+    document.body.appendChild(this.root);
+}
+
+componentWillUnmount() {
+    document.body.removeChild(this.root);
+}
+
   handleClick = () => {
     const { handleVideoClick } = this.props;
     handleVideoClick();
   };
 
-  _onReady(event) {
-    event.target.pauseVideo();
-  }
-
   render() {
     const { stateVideo, video } = this.props;
     const opts = {
-      height: '390',
-      width: '640',
       playerVars: {
         autoplay: 1,
       },
@@ -30,9 +33,9 @@ class Video extends Component {
           stateVideo ? ReactDOM.createPortal(
             <div className="buttonVideo">
               <button type="button" onClick={this.handleClick}>back</button>
-              <YouTube videoId={video[0].key} opts={opts} onReady={this._onReady} />
+              <YouTube videoId={video[0].key} opts={opts} />
             </div>,
-            document.getElementById('app')) : null}
+            this.root) : null}
       </div>
     );
   }
