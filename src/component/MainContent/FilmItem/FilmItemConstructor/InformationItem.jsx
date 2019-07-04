a309e825603bcf769c5ae915ba58ed6bb370b70d
IgorNichiporukCristo <inichiporuk1992@gmail.com>
2019-07-05 00:47:26 +0300

    some update

--- src/component/MainContent/FilmItem/FilmItemConstructor/InformationItem.jsx
+++ src/component/MainContent/FilmItem/FilmItemConstructor/InformationItem.jsx
@@ -1,18 +1,50 @@
-import React from 'react';
+import React,{ Component } from 'react';
 import PropTypes from 'prop-types';
 import './informationItem.scss';
 
-const InformationItem = ({ title, vote, genres }) => (
-  <div className="information-countainer">
-    <div className="information-title-vote">
-      <h4 className="information-title">{title}</h4>
-      <h4 className="information-vote">{vote}</h4>
-    </div>
-    <h5 className="information-genres">
-      {genres? genres.slice(0, 3).map(obj => obj.name).join(', '): 'Update page'}
-    </h5>
-  </div>
-);
+class InformationItem extends Component{
+  componentDidMount(){
+    
+  }
+
+  handleClick = () => {
+    return console.log("sdgsdg");
+  }  
+
+  render(){
+    const {title, vote, genres} = this.props;
+    return(
+      <div 
+        className="information-countainer" 
+        onClick={this.handleClick} 
+        onKeyPress={this.handleClick}     
+        role="button" 
+        tabIndex="0"
+      >
+        <div className="information-title-vote">
+          <h4 className="information-title">{title}</h4>
+          <h4 className="information-vote">{vote}</h4>
+        </div>
+        <h5 className="information-genres">
+          {genres? genres.slice(0, 3).map(obj => obj.name).join(', '): 'Update page'}
+        </h5>
+      </div>
+    );
+  }
+
+}
+
+function mapStateToProps(state) {
+  return {
+    items: state.items,
+  };
+}
+
+function mapDispatchToProps(dispatch, { item: { id } = null }) {
+  return {
+    getDescriptionFilm: () => dispatch(getDescriptionFilm(id)),
+  };
+}
 
 InformationItem.defaultProps = {
   genres: [],
@@ -23,4 +55,7 @@ InformationItem.propTypes = {
   genres: PropTypes.arrayOf(PropTypes.object),
 };
 
-export default InformationItem;
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(InformationItem);
