faa96b049ca0985b6beee6cd96d964a840d53d62
IgorNichiporukCristo <inichiporuk1992@gmail.com>
2019-10-10 22:16:25 +0300

    added search

--- src/Content/Header/HeaderSearch/HeaderSearch.jsx
+++ src/Content/Header/HeaderSearch/HeaderSearch.jsx
@@ -2,15 +2,32 @@ import React, { Component } from 'react';
 import './headersearch.scss';
 
 class HeaderSearch extends Component  {
- componentDidMount() {}
+ state = {
+   showsearch: false,
+ }
+
+  handleClick = () => {
+    this.setState({showsearch: true});
+  }
+
   render() {
+    const { showsearch } = this.state;
     return (
       <div className="header-name-search">
         <h1 className="header-name">Igor_ZBS_PACAN</h1>
-        <input 
-          className="header-search" 
-          placeholder="Search movies by name..." 
-        />
+        <div className="search-block">
+          <input 
+            className="header-search" 
+            placeholder="Search movies by name..." 
+            onClick={this.handleClick}
+          />
+          {showsearch ?(
+            <div className="show-search">
+              <span>fsedwfe</span>
+            </div>
+          ): null
+          }
+        </div>
       </div>
     );
   }
